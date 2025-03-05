56221743983d14c4fb02f385ff097f2b70332449
Aizen09 <scilentgamer99@gmail.com>
2025-03-05 14:37:04 +0530

    done

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div style={{textAlign : 'center', marginTop :'50px'}}>
+            <h1>Mohit Say's Yo Hello</h1>
+            <p>Eamil: mohit@gmail.com</p>
+            <p>Phone: +91 90025 90615</p>
+        
+        </div>
+    );
+};
+
+export default Home;
+
+
